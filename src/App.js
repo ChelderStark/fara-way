85366c386d7ebcb4c121f0f82c8ddcab2c8059f7
ChelderStark <helderdroid@gmail.com>
2025-01-06 09:22:15 -0300

    it has done the actions list to sort items.

--- src/App.js
+++ src/App.js
@@ -76,14 +76,32 @@ function Form({onAddItems}) {
   )
 }
 
-function PackingList({items, onDeleteItem, onToggleItems}) { 
+function PackingList({items, onDeleteItem, onToggleItems}) {
+  const[sortBy, setSortBy] = useState("input")
+
+  let sortedItems;
+
+  if(sortBy === "input") sortedItems = items;
+
+  if(sortBy === 'description') sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description))
+
+  if(sortBy === 'packed') sortedItems = items.slice().sort((a,b) => Number(b.packed) - Number(a.packed))
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item item={item} onDeleteItem={onDeleteItem} onToggleItems={onToggleItems} key={item.id}/>
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+        <button>Clear list</button>
+      </div>
     </div>
   )
 }
